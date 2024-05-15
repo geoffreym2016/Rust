acefc81eba72dd77fc4f5d5990ef7d8e9b841550
Geoffrey Mutisya <geoffreym2016@gmail.com>
2024-05-16 01:48:15 +0300

    If keyword let is used for the same variable shadowing happens untill end of the scope. Check compiler.

---
+++ my_rust_projects/shadowing/target/debug/deps/shadowing.d
@@ -0,0 +1,5 @@
+C:\Users\Administrator\Rust\my_rust_projects\shadowing\target\debug\deps\shadowing.exe: src\main.rs
+
+C:\Users\Administrator\Rust\my_rust_projects\shadowing\target\debug\deps\shadowing.d: src\main.rs
+
+src\main.rs:
---
+++ my_rust_projects/shadowing/target/debug/shadowing.d
@@ -0,0 +1,1 @@
+C:\Users\Administrator\Rust\my_rust_projects\shadowing\target\debug\shadowing.exe: C:\Users\Administrator\Rust\my_rust_projects\shadowing\src\main.rs
