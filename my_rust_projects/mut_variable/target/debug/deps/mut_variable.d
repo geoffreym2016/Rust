ae37f25ce30c3028258f1e865572e58807b4cf92
Geoffrey Mutisya <geoffreym2016@gmail.com>
2024-05-16 01:27:23 +0300

    mut_variable runs because of keyword mut which allows change of ownership

---
+++ my_rust_projects/mut_variable/target/debug/deps/mut_variable.d
@@ -0,0 +1,5 @@
+C:\Users\Administrator\Rust\my_rust_projects\mut_variable\target\debug\deps\mut_variable.exe: src\main.rs
+
+C:\Users\Administrator\Rust\my_rust_projects\mut_variable\target\debug\deps\mut_variable.d: src\main.rs
+
+src\main.rs:
---
+++ my_rust_projects/mut_variable/target/debug/mut_variable.d
@@ -0,0 +1,1 @@
+C:\Users\Administrator\Rust\my_rust_projects\mut_variable\target\debug\mut_variable.exe: C:\Users\Administrator\Rust\my_rust_projects\mut_variable\src\main.rs
