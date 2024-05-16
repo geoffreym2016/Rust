ea7a3109dc8df7aef0978a08643e2aee41497f25
Geoffrey Mutisya <geoffreym2016@gmail.com>
2024-05-16 10:44:11 +0300

    Tuple data type groups together a variety ov values into one compound type

---
+++ my_rust_projects/tuple_data_type/target/debug/deps/tuple_data_type.d
@@ -0,0 +1,5 @@
+C:\Users\Administrator\Rust\my_rust_projects\tuple_data_type\target\debug\deps\tuple_data_type.exe: src\main.rs
+
+C:\Users\Administrator\Rust\my_rust_projects\tuple_data_type\target\debug\deps\tuple_data_type.d: src\main.rs
+
+src\main.rs:
---
+++ my_rust_projects/tuple_data_type/target/debug/tuple_data_type.d
@@ -0,0 +1,1 @@
+C:\Users\Administrator\Rust\my_rust_projects\tuple_data_type\target\debug\tuple_data_type.exe: C:\Users\Administrator\Rust\my_rust_projects\tuple_data_type\src\main.rs
