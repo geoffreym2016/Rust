d9193f3de4d989c69f07c6a852b5b054a8cecbef
Geoffrey Mutisya <geoffreym2016@gmail.com>
2024-05-22 02:43:23 +0300

    Functions use snake case style. Can be called from anywhere as long as caller sees the scope.

---
+++ my_rust_projects/functions/target/debug/deps/functions.d
@@ -0,0 +1,5 @@
+C:\Users\Administrator\Rust\my_rust_projects\functions\target\debug\deps\functions.exe: src\main.rs
+
+C:\Users\Administrator\Rust\my_rust_projects\functions\target\debug\deps\functions.d: src\main.rs
+
+src\main.rs:
---
+++ my_rust_projects/functions_with_parameters/target/debug/deps/functions_with_parameters.d
@@ -0,0 +1,5 @@
+C:\Users\Administrator\Rust\my_rust_projects\functions_with_parameters\target\debug\deps\functions_with_parameters.exe: src\main.rs
+
+C:\Users\Administrator\Rust\my_rust_projects\functions_with_parameters\target\debug\deps\functions_with_parameters.d: src\main.rs
+
+src\main.rs:
